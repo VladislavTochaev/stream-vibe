3c98dc58353d1f43ce720cf1bfa3f7cc4c375a75
Vladislav Tochaev <vlad.tochaev2021@gmail.com>
2025-08-03 17:09:57 +0400

    feat: component MovieBannerCard

---
+++ src/sections/MoviesBanner/MoviesBanner.jsx
@@ -0,0 +1,20 @@
+import './MoviesBanner.scss'
+import Section from '@/layouts/Section'
+
+const MoviesBanner = () => {
+  const titleId = 'movies-banner-title'
+
+  return (
+    <section
+      className="movies-banner container"
+      aria-labelledby={titleId}
+    >
+      <h1 className="visually-hidden" id={titleId}>
+        Movies & Shows
+      </h1>
+
+    </section>
+  )
+}
+
+export default MoviesBanner
